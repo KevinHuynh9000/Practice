5d1ff05b7dcbd66c80801b83df5321dc094f0474
kevinhuynh9000 <87397546+KevinHuynh9000@users.noreply.github.com>
2021-07-15 21:11:04 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+Dalist = ["12", "42", "13", "96"]
+print ("Max value : ", max(Dalist))
+
+x= 25
+y= 12
+z= 30
+v= 67
+add= x+y+z+v
+print(add)
+
+Danumbers=[1,2,3,4,5,6,7,8,9,10]
+for num in Danumbers:
+  if num % 2 == 0:
+      print(num, end = " ")
+
+#Kevin, Anthony, Gabby
